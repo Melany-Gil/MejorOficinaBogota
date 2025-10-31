483b74dd7c98839869a870e8ff610ff18e2bb4ab
Melany-Gil <melanygilgrau@gmail.com>
2025-10-31 14:12:07 -0500

    commit

--- script/candidates-decoraciones.js
+++ script/candidates-decoraciones.js
@@ -1,7 +1,7 @@
 // Opciones (decoraciones de oficinas - solo título)
 const CANDIDATES = [
-  { id: "decor01", name: "Años Dorados" },
-  { id: "decor02", name: "Alicia en el País de las Maravillas" },
-  { id: "decor03", name: "Monsters, Inc." },
+  { id: "decor01", name: "Años Dorados\n Oficina 214" },
+  { id: "decor02", name: "Alicia en el País de las Maravillas\n Oficina 209" },
+  { id: "decor03", name: "Monsters, Inc.\n Oficina 213" },
   { id: "blank",   name: "Voto en blanco", blank: true }
 ];
